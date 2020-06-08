844f03c0bc66411e941139ab135bdc17d12d36d7
aayushhindwan <aayushindwan@email.com>
2020-06-08 14:11:11 +0530

    Updates

---
+++ precache-manifest.6d053ae4a6c37ca56093d74cef0b5c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8f2c8baea147828dd9ad34157627b7",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "a2b89cfbbed1f5f6a05e",
+    "url": "/Tic-Tac-Toe/static/css/main.98af0005.chunk.css"
+  },
+  {
+    "revision": "ad1fba5fe4cd8d47d46e",
+    "url": "/Tic-Tac-Toe/static/js/2.1f56cec4.chunk.js"
+  },
+  {
+    "revision": "a2b89cfbbed1f5f6a05e",
+    "url": "/Tic-Tac-Toe/static/js/main.b2a83148.chunk.js"
+  },
+  {
+    "revision": "bc7c4b92b2cee2200306",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.d364f09a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-Tac-Toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
